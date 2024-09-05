024b1cf2de52f1d6174ad18ab3dcfba6ec360721
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-09-05 12:06:12 +0200

    Added colors

--- papRocScis.c
+++ papRocScis.c
@@ -8,6 +8,16 @@ int getUserInput();
 int getComputerInput();
 int startGame();
 
+void red();
+void green();
+void cyan();
+void blue();
+void yellow();
+void white();
+void black();
+void printChoice(int choice);
+void printDottedLine();
+
 int main()
 {
     startGame();
@@ -17,22 +27,33 @@ int main()
 int getUserInput()
 {
     char *input = malloc(sizeof(char));
-    printf("----------------------------------------\n\tRock, Paper, Scissors ?\n\t");
-    scanf("%s", input);
 
+    printDottedLine();
+
+    red();
+    printf("\tRock, Paper, Scissors ?\n\t");
+    white();
+    cyan();
+    scanf("%s", input);
+    white();
+
+    printDottedLine();
     if (strcmp("Rock", input) == 0 || strcmp("rock", input) == 0)
     {
-        printf("----------------------------------------\n\tYou chose %s\n", input);
+        printf("\tYou chose ");
+        printChoice(1);
         return 1;
     }
     else if (strcmp("Paper", input) == 0 || strcmp("paper", input) == 0)
     {
-        printf("----------------------------------------\n\tYou chose %s\n", input);
+        printf("\tYou chose ");
+        printChoice(2);
         return 2;
     }
     else if (strcmp(input, "Scissors") == 0 || strcmp(input, "scissors") == 0)
     {
-        printf("----------------------------------------\n\tYou chose %s\n", input);
+        printf("\tYou chose ");
+        printChoice(2);
         return 3;
     }
     else
@@ -48,20 +69,23 @@ int getComputerInput()
     int randNum = rand() % 9;
     if (randNum < 3)
     {
-        printf("\tComputer chose Rock\n");
-        printf("----------------------------------------\n");
+        printf("\tComputer chose ");
+        printChoice(1);
+        printDottedLine();
         return 1;
     }
     else if (randNum < 6)
     {
-        printf("\tComputer chose Paper\n");
-        printf("----------------------------------------\n");
+        printf("\tComputer chose ");
+        printChoice(2);
+        printDottedLine();
         return 2;
     }
     else
     {
-        printf("\tComputer chose Scissors\n");
-        printf("----------------------------------------\n");
+        printf("\tComputer chose ");
+        printChoice(3);
+        printDottedLine();
         return 3;
     }
 }
@@ -82,4 +106,68 @@ int startGame()
     {
         printf("\tYou WON!!!\n");
     }
+}
+
+void red()
+{
+    printf("\033[1;31m");
+}
+
+void green()
+{
+    printf("\033[0;32m");
+}
+
+void yellow()
+{
+    printf("\033[0;33m");
+}
+
+void white()
+{
+    printf("\033[0m");
+}
+
+void black()
+{
+    printf("\033[0;30m");
+}
+
+void cyan()
+{
+    printf("\033[0;36m");
+}
+
+void blue()
+{
+    printf("\033[0;34m");
+}
+
+void printChoice(int choice)
+{
+    if (choice == 1)
+    {
+        yellow();
+        printf("Rock\n");
+        white();
+    }
+    else if (choice == 2)
+    {
+        cyan();
+        printf("Paper\n");
+        white();
+    }
+    else
+    {
+        blue();
+        printf("Scissors\n");
+        white();
+    }
+}
+
+void printDottedLine()
+{
+    red();
+    printf("----------------------------------------\n");
+    white();
 }
